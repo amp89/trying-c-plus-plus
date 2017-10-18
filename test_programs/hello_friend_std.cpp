390a99edc340f3855e61dbd764dcf430d7af7da3
Alex <alexander.peterson89@gmail.com>
2017-10-18 00:08:01 -0400

    testing out variable declaration in c++

--- test_programs/hello_friend_std.cpp
+++ test_programs/hello_friend_std.cpp
@@ -10,4 +10,30 @@ int main()
 
     std::cout << "printing some more stuff...";//looks like this works too
 
+    //playing around with variables
+    int a = 5;
+    int b = 123;
+    int c = a + b;
+    cout << c;
+
+    int z,x;
+
+
+    int a2 = 5;
+    int b2(23);
+    int c2 {3};
+    int d2{403};
+    auto e = d2; //automatically figure it out
+    decltype(d2) result; //get the type of
+
+    result = a2+b2+c2+d2;
+    cout << "\n";
+    cout << result;
+    cout << "\n";
+
+    cout << "\n";
+
+
+
+
 }
