81c9c2967c003e564b5991d68c0fae5cb902a2d8
Alex <alexander.peterson89@gmail.com>
2017-10-17 23:05:34 -0400

    using namespace std

---
+++ test_programs/hello_friend_std.cpp
@@ -0,0 +1,13 @@
+//my first c++ program, how neat!
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //std::cout << "Hello friend.";//standard character output device
+    cout << "\nHello friend.\n";//standard character output device
+
+    std::cout << "printing some more stuff...";//looks like this works too
+
+}
